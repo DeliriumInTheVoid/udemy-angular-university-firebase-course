e86e5677c6196139a317a90f1206b0fd6c62b40c
ilia volkov <ivolkov@playtika.com>
2025-07-02 11:18:29 +0300

    production deployment with firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyAKFOTY4d_l4fb_43yHmAhjUzC26jGoaW0",
+    authDomain: "udemy-firebase-in-depth-a486f.firebaseapp.com",
+    projectId: "udemy-firebase-in-depth-a486f",
+    storageBucket: "udemy-firebase-in-depth-a486f.firebasestorage.app",
+    messagingSenderId: "828138133707",
+    appId: "1:828138133707:web:ffc6f7ed974ef57b01634e"
+  },
+  api: {
+    createUser: "https://createuser-7lro3l7vba-uc.a.run.app",
+  }
 };
